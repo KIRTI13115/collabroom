5ec3af1ae66d34c660129ae7b1bd54ba682474b7
KIRTI13115 <192kirti@gmail.com>
2026-04-21 21:48:54 +0530

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const protectedRoute = createRouteMatcher([
-  '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
